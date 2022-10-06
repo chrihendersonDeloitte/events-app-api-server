9fb01a4db97782766977c5113ec4050df0ac4d30
chrihendersonDeloitte <chrihenderson@deloitte.com>
2022-10-06 16:57:38 +0000

    some message

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team awsuser11';
+    team = 'Team awsuser11 XX';
     version = '1.0.1';
 }
 module.exports = AppConfig;
